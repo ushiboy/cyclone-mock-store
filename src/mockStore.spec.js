60a75d7350480b74040f5a4191a1f7635a942747
ushiboy <ushiboy.dev@gmail.com>
2019-03-26 23:05:05 +0900

    default update case

--- src/mockStore.spec.js
+++ src/mockStore.spec.js
@@ -9,15 +9,42 @@ describe('createMockStore', function() {
   context('default update', () => {
     let store;
     beforeEach(() => {
-      store = createMockStore()(initState());
+      store = createMockStore(initState());
     });
     describe('dispatch', () => {
-      it('should push action history', () => {
-        store.dispatch({ type: 'increment' });
+      it('should push action history', async () => {
+        await store.dispatch({ type: 'increment' });
         const actions = store.getActions();
         assert(actions.length === 1);
         const [a] = actions;
         assert(a.type === 'increment');
+      });
+    });
+    describe('getState', () => {
+      it('should return the state of store', () => {
+        const state = store.getState();
+        assert(state.count === 0);
+      });
+    });
+    describe('getActions', () => {
+      it('should return action histories', async () => {
+        await store.dispatch({ type: 'increment' });
+        await store.dispatch({ type: 'decrement' });
+        const actions = store.getActions();
+        assert(actions.length === 2);
+        const [a1, a2] = actions;
+        assert(a1.type === 'increment');
+        assert(a2.type === 'decrement');
+      });
+    });
+    describe('clearActions', () => {
+      it('should clear action histories', async () => {
+        await store.dispatch({ type: 'increment' });
+        await store.dispatch({ type: 'decrement' });
+        assert(store.getActions().length === 2);
+        store.clearActions();
+        const actions = store.getActions();
+        assert(store.getActions().length === 0);
       });
     });
   });
